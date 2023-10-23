786d1fbe0a88bf7c66d1a31fcc604ee5cbd0690f
Jéssica Sobreira <117686537+jessica-sobreira@users.noreply.github.com>
2023-10-23 14:18:09 -0300

    Update Home.ts

--- src/components/Home.ts
+++ src/components/Home.ts
@@ -1,58 +1,59 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
-main::after{
+  position: relative;
+
+  ::after {
     content: "";
     position: fixed;
     top: 0;
     left: 0;
     min-height: 100vh;
     width: 80vw;
-    background: linear-gradient(-233deg, #000 20%, rgba(0, 0, 0, 0) 65%) no-repeat;
-}
+  }
 
-main .student {
+  .student {
     display: none;
     height: 100vh;
-}
+  }
 
-main .student.selected{
+  .student.selected {
     display: block;
-}
+  }
 
-main .student .image{
+  .student .image {
     width: 100%;
     height: 100%;
     object-fit: cover;
-}
-`
+  }
+`;
 
 export const Title = styled.h1`
-    color: white;
-    display: flex;
-    margin-top: 13%;
-    margin-left: 16%;
-    font-size: 3em;
-`
+  color: white;
+  display: flex;
+  margin-top: 13%;
+  margin-left: 16%;
+  font-size: 3em;
+`;
 
 export const Text = styled.p`
-    color: white;
-    display: flex;
-    margin-left: 4%;
-    margin-top: 2%;
-    font-size: 1.5em;
-    flex-direction: column;
-    margin-right: 45%;
-    text-align: center;
-    `
-    export const Button = styled.button`
-    border: none;
-    background-color: transparent;
-    cursor: pointer;
-    
-  `;
+  color: white;
+  display: flex;
+  margin-left: 4%;
+  margin-top: 2%;
+  font-size: 1.5em;
+  flex-direction: column;
+  margin-right: 45%;
+  text-align: center;
+`;
 
-  export const Buttons = styled.div`
+export const Button = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+export const Buttons = styled.div`
   position: fixed;
   top: 0;
   right: 0;
@@ -64,28 +65,18 @@ export const Text = styled.p`
   align-items: center;
   gap: 18px;
   background-color: rgba(0, 0, 0, 0.3);
+  list-style: none;
 
-  ul {
-    list-style: none;
-}
-
-  
-  Buttons Button {
+  ${Button} {
     border: none;
     background: #000000;
     cursor: pointer;
     border-radius: 50%;
-}
+  }
 
-Button img {
+  ${Button} img {
     border-radius: 50%;
     height: 2%;
     width: 7vw;
-
-
-}
-
-
-  `
-
-
+  }
+`;
