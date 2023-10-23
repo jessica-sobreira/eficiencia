3b688cb1e728459aa47a74e1190a8c6a90c61ec5
Jéssica Sobreira <117686537+jessica-sobreira@users.noreply.github.com>
2023-10-23 15:15:33 -0300

    Update Gabriel.tsx

--- src/pages/Gabriel.tsx
+++ src/pages/Gabriel.tsx
@@ -1,4 +1,6 @@
 import { Student, Content, Description, NameStudent, ImageStudent, StyledLink } from '../components/Students';
+import { Link } from 'react-router-dom';
+import { Button, Buttons } from '../components/Home';
 
 
 export function Gabriel() {
@@ -19,6 +21,24 @@ export function Gabriel() {
             </Description>
           </Content>
         </Student>
+        <Buttons>
+        <li>
+      <Link to='/gabriel'><Button><img src="https://avatars.githubusercontent.com/u/111029467?v=4" alt="Gabriel" /></Button></Link>
+      </li>
+      <li>
+      <Link to='/jessica'><Button><img src="https://avatars.githubusercontent.com/u/117686537?v=4" alt="Jéssica" /></Button></Link>
+      </li>
+      <li>
+      <Link to='/stephan'><Button><img src="https://avatars.githubusercontent.com/u/109179158?v=4" alt="Stefan" /></Button></Link>
+      </li>
+      <li>
+      <Link to='/tanara'><Button><img src="https://media.licdn.com/dms/image/C4D03AQHY69elQf-D2A/profile-displayphoto-shrink_800_800/0/1630435860458?e=1703721600&v=beta&t=87zcDroa84K_anmSZL-IY49DZZroeDyemqRVELLrimQ" alt="Tanara" /></Button></Link>
+      </li>
+      <li>
+      <Link to='/yuri'><Button><img src="https://avatars.githubusercontent.com/u/127044623?v=4" alt="Yuri" /></Button></Link>
+      </li>
+      </Buttons>
+
 
 
         </>
